3788011526afc6f33c99a2745e2145e7a5335881
Jing Li <lyovse@gmail.com>
2019-04-10 18:32:39 +1000

    Add a method to update array size

--- AdjList.java
+++ AdjList.java
@@ -65,4 +65,19 @@ public class AdjList extends AbstractAssocGraph {
         // Implement me!
     } // end of printEdges()
 
+    private void updateArrayLength() {
+        int currentLen = rows.length;
+        if (currentLen - numOfVerts <= 5) {
+            // Increase array size by 25 when the difference between current size and number of vertices
+            // is smaller than or equal to 5
+            int newLen = currentLen + 25;
+            rows = Arrays.copyOf(rows, newLen);
+        } else if (currentLen - numOfVerts >= 55) {
+            // Decrease array size by 25 when the difference between current size and number of vertices
+            // is greater than or equal to 55
+            int newLen = currentLen - 25;
+            rows = Arrays.copyOf(rows, newLen);
+        }
+    }
+
 } // end of class AdjList
