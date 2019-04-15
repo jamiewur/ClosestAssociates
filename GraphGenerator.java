b475a3e14e037a88d380ccbb7b5c423892bf18a4
Jing Li <lyovse@gmail.com>
2019-04-15 13:02:30 +1000

    Create a method to calculate number of edges according to density

--- GraphGenerator.java
+++ GraphGenerator.java
@@ -5,7 +5,10 @@ public class GraphGenerator {
     private AbstractAssocGraph graph;
     private Density density;
     private int numOfVerts;
+    private int numOfEdges;
+    private int maxNumEdges;
     private int[] vertices;
+
     private enum Density {
         LOW,
         MEDIUM,
@@ -14,8 +17,19 @@ public class GraphGenerator {
 
     public GraphGenerator(int numOfVerts, Density density) {
         this.numOfVerts = numOfVerts;
+        this.maxNumEdges = numOfVerts * (numOfVerts - 1);
         this.density = density;
         this.vertices = IntStream.range(1, numOfVerts + 1).toArray();
+    }
+
+    private int calculateNumOfEdges() {
+        if (density == Density.HIGH) {
+            return maxNumEdges;
+        } else if (density == Density.MEDIUM) {
+            return (int) (maxNumEdges * 0.66);
+        } else {
+            return (int) (maxNumEdges * 0.33);
+        }
     }
 
 
